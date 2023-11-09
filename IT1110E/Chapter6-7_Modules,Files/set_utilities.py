d279c4f92a4065d00a3da41a3ff9d6ade4fff97f
Supporter09 <maivannhatminh2005@gmail.com>
2023-11-09 09:56:12 +0700

    Fix

--- IT1110E/Chapter6-7_Modules,Files/set_utilities.py
+++ IT1110E/Chapter6-7_Modules,Files/set_utilities.py
@@ -1,19 +1,5 @@
-def set_union(A,B):
-    res = set()
-    for el in A:
-        res.add(el)
-    for el in B: 
-        res.add(el)
-    return res
+def set_union(A, B):
+  return A | B
 
 def set_intersection(A, B):
-    res = set()
-    if len(A) >= len(B):
-        for el in A:
-            if el in B:
-                res.add(el)
-    else:
-        for el in B:
-            if el in A:
-                res.add(el)
-    return res
+  return A & B
